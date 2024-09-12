18361777acba200b6d8aaf5b53ec023938f883d8
Tais <taiscoliveiratec@gmail.com>
2024-09-12 14:12:43 -0300

    back-end

---
+++ server/src/http/server.ts
@@ -0,0 +1,36 @@
+import fastify from "fastify";
+import dotenv from "dotenv";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { createGoalRoute } from "./routes/create-goal";
+import { createCompletionRoute } from "./routes/create-completion";
+import { getPendingGoalsRoute } from "./routes/get-pending-goals";
+import { getWeekSummaryRoute } from "./routes/get-week-summary";
+import fastifyCors from "@fastify/cors";
+
+dotenv.config();
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+	origin: "*",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createGoalRoute);
+app.register(createCompletionRoute);
+app.register(getPendingGoalsRoute);
+app.register(getWeekSummaryRoute);
+
+app
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log("HTTP server running");
+	});
