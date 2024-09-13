9f6d13f6aec1908e5e6b75f774e5247d9e7c8741
Tais <taiscoliveiratec@gmail.com>
2024-09-13 14:32:15 -0300

    Ajuste no back-end e quarta página

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,12 +1,20 @@
 import { CreateGoal } from "./components/create-goal";
 import { Summary } from "./components/summary";
-//import { EmptyGoals } from "./components/empty-goals";
+import { EmptyGoals } from "./components/empty-goals";
 import { Dialog } from "./components/ui/dialog";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "./http/get-summary";
 
 export function App() {
+	const { data } = useQuery({
+		queryKey: ["summary"],
+		queryFn: getSummary,
+		staleTime: 1000 * 60,
+	});
+
 	return (
 		<Dialog>
-			{/*<EmptyGoals />*/}
+			{data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 			<Summary />
 			<CreateGoal />
 		</Dialog>
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,14 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
 	const root = createRoot(rootElement);
+
 	root.render(
 		<StrictMode>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
 		</StrictMode>,
 	);
 }
